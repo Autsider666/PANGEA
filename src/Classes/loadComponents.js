8e4707d02b67e1f2f8ce77bbfb9b4613ae8e1686
Yorick van Klinken <yorickvanklinken@gmail.com>
2018-07-09 19:29:31 +0200

    ECS test 2

--- src/Classes/loadComponents.js
+++ src/Classes/loadComponents.js
@@ -1,25 +1,25 @@
-// import Component from './ECS/Component'
-//
-// Component.extend('renderable', type => type)
-//
-// Component.extend('brain', brain => brain)
-//
-// Component.extend('location', (x, y, world) => {
-//     return {x: x, y: y, world: world}
-// })
-//
-// Component.extend('size', (height, width) => {
-//     return {height: height, width: width}
-// })
-//
-// Component.extend('grid', (height, width) => {
-//     return {height: height, width: width}
-// })
-//
-// Component.extend('energy', amount => amount)
-//
-// Component.extend('predator')
-// Component.extend('prey')
-// Component.extend('world', name => name)
-//
-// Component.extend('element', element => element)
+import Component from './ECS/Component'
+
+Component.extend('renderable', type => type)
+
+Component.extend('brain', brain => brain)
+
+Component.extend('location', (x, y, world) => {
+    return {x: x, y: y, world: world}
+})
+
+Component.extend('size', (height, width) => {
+    return {height: height, width: width}
+})
+
+Component.extend('grid', (height, width) => {
+    return {height: height, width: width}
+})
+
+Component.extend('energy', amount => amount)
+
+Component.extend('predator')
+Component.extend('prey')
+Component.extend('world', name => name)
+
+Component.extend('element', element => element)
